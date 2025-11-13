d02bc5e64df78ad62f967d3a32b7fe4c9180b3cb
Enrique Uribe <ukikke@gmail.com>
2025-11-13 23:54:34 +0000

    finished

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,8 @@
 # Funtions below
+products = [{'Product': 'Snickers', 'Price': 1.99},{'Product': 'Case of 40 waters', 'Price': 5.38} , {'Product': 'Single water bottle', 'Price': 0.75}, {'Product': 'Noodle Cups', 'Price': 1.82}]
 
 def beginning():
-    print("Welcome to Elite 101 Chatbot!")
+    print("Welcome to HEB chat bot!")
     name = input("What is your name? ")
     age = input(f"Nice to meet you, {name}! How old are you? ")
     print(f"Welcome {name}! Wow you're {age}! You are so young! How can I help you today?")
@@ -9,19 +10,43 @@ def beginning():
 
 def display_menu():
     print("\n Please choose from the Please choose from the following options:")
-    print(" 1. Place holder for option 1\n 2. Place holder for option 2\n 3. Place holder for option 3\n 4. Exit Conversation")
+    print(" 1. Store Hours \n 2. Closes location to you \n 3. available products \n 4. Prices \n 5. Exit Conversation")
 
 def selection(name):
-    number = int(input('Enter the number of your choice: '))
+    number = int(input('Enter the number of your choice[1-5]:  '))
     if number == 1:
-        print("Place holder for option 1")
+        store_hourly()
     elif number == 2:
-        print("Place holder for option 2")
+        location()
     elif number == 3:
-        print("Place holder for option 3")
+        print_products()
     elif number == 4:
+        price_of_product()
+    elif number == 5:
         print(f"Have a great day! Goodbye, {name}!")
         return 1
+
+
+def store_hourly():
+    print('All store hours are from 6:00 AM till 11 PM')
+
+def location():
+    print('The closest location is at 10100 Beechnut St')
+
+def print_products():
+    for i in range(len(products)):
+        print("---------------------")
+        print(f"Product:{products[i]['Product']}")
+    print("-------------------")
+
+def price_of_product():
+    for item in products:
+        print("---------------------")
+        for key, value in item.items():
+            print(f"{key}:{value}")
+    print("-------------------")
+
+
 
 #commands below
 
